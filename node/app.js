5b5725af7353be9e7299137151798c0b47263df8
dychko <dichko.victor@gmail.com>
2017-04-05 14:58:57 +0200

    Fix timeout

--- node/app.js
+++ node/app.js
@@ -334,8 +334,16 @@ function approveLocation(senderID, message) {
 }
 
 function findVehicles() {
-  setTimeout(function() {
-  }, 1000);
+  function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
+
+  sleep(1000);
 }
 
 
